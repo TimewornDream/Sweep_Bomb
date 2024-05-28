f8ddf02fff28496c16c80911b1ba4ec62ffd3855
TimewornDream <2497505099@qq.com>
2024-05-28 18:07:15 +0800

    feat:实现窗口

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event->{
-            System.out.println("hello world");
-        });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+       primaryStage.setTitle("扫雷");
+       primaryStage.setScene(new Scene(new StackPane(), 300, 300));
+       primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
