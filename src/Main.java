39e5f4bf145f13ae17fe9a345800d993d88d9348
TimewornDream <2497505099@qq.com>
2024-05-29 00:51:29 +0800

    feat:完成主界面背景

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -31,6 +33,20 @@ public class Main extends Application {
                 "-fx-padding: 10px"
         );
 
+        // 中间三个圆
+        StackPane center = new StackPane();
+        Circle circle1 = new Circle(319);
+        circle1.setFill(Paint.valueOf("#F2F3F5"));
+        circle1.setTranslateY(180);
+
+        Circle circle2 = new Circle(319);
+        circle2.setFill(Paint.valueOf("#E8EAED"));
+        circle2.setTranslateY(240);
+
+        Circle circle3 = new Circle(319);
+        circle3.setFill(Paint.valueOf("#0F1F41"));
+        circle3.setTranslateY(300);
+
         // 中间三个按钮
         MainButton junButton = new MainButton("初级", 240, 240);
         MainButton midButton = new MainButton("中级", 480, 480);
@@ -40,14 +56,19 @@ public class Main extends Application {
         midButton.initStyle();
         senButton.initStyle();
 
-        VBox center = new VBox();
-        center.getChildren().addAll(junButton, midButton, senButton);
-        center.setAlignment(Pos.CENTER);
-        center.setSpacing(20);
+        VBox buttons = new VBox();
+        buttons.getChildren().addAll(junButton, midButton, senButton);
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setSpacing(20);
+        buttons.setTranslateY(150);
+
+        center.getChildren().addAll(circle1, circle2, circle3, buttons);
 
         // 添加各部分
         root.setTop(top);
         root.setCenter(center);
+        root.setMinWidth(scene.getWidth());
+        root.setMinHeight(scene.getHeight());
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
