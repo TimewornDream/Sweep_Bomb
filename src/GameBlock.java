4e2d59da8ae38707345e17076c769bcf55196f23
TimewornDream <2497505099@qq.com>
2024-05-30 23:40:46 +0800

    feat:完成雷区点击和悬停的样式变化

--- src/GameBlock.java
+++ src/GameBlock.java
@@ -1,11 +1,44 @@
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
-public class GameBlock extends Rectangle {
+public class GameBlock extends StackPane {
+    private int status = 0;
     GameBlock(){
-        super(39, 39);
-        this.setFill(Color.BLUE);
-        this.setStroke(Color.ALICEBLUE);
-        this.setStrokeWidth(1);
+        super();
+        this.setMaxSize(40, 40);
+
+        ImageView block = new ImageView("./img/block.png");
+        block.setFitWidth(40);
+        block.setFitHeight(40);
+        ImageView cursor = new ImageView("./img/cursor_block.png");
+        cursor.setFitWidth(40);
+        cursor.setFitHeight(40);
+
+        this.getChildren().add(block);
+
+        // block 点击变换图像
+        this.setOnMouseClicked( e->{
+                ImageView empty = new ImageView("./img/empty_block.png");
+                empty.setFitHeight(40);
+                empty.setFitWidth(40);
+                this.getChildren().clear();
+                this.getChildren().add(empty);
+                this.status = 1;
+            }
+        );
+
+        // block 悬停变换图像
+        this.setOnMouseEntered( e->{
+            if(status == 0) {
+                this.getChildren().clear();
+                this.getChildren().add(cursor);
+            }
+        });
+        this.setOnMouseExited( e->{
+            if(status == 0){
+                this.getChildren().clear();
+                this.getChildren().add(block);
+            }
+        });
     }
 }
