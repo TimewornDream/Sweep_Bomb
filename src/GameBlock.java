2299a2d197831bf917b5bdcc16471520cc901bbc
TimewornDream <2497505099@qq.com>
2024-05-31 22:04:58 +0800

    fix:修正了鼠标事件的显示效果

--- src/GameBlock.java
+++ src/GameBlock.java
@@ -1,8 +1,10 @@
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
 
 public class GameBlock extends StackPane {
     private int status = 0;
+    private boolean isPress = false;
     GameBlock(){
         super();
         this.setMaxSize(40, 40);
@@ -13,31 +15,40 @@ public class GameBlock extends StackPane {
         ImageView cursor = new ImageView("./img/cursor_block.png");
         cursor.setFitWidth(40);
         cursor.setFitHeight(40);
+        ImageView empty = new ImageView("./img/empty_block.png");
+        empty.setFitHeight(40);
+        empty.setFitWidth(40);
 
         this.getChildren().add(block);
 
-        // block 点击变换图像
-        this.setOnMouseClicked( e->{
-                ImageView empty = new ImageView("./img/empty_block.png");
-                empty.setFitHeight(40);
-                empty.setFitWidth(40);
-                this.getChildren().clear();
-                this.getChildren().add(empty);
-                this.status = 1;
-            }
-        );
 
         // block 悬停变换图像
-        this.setOnMouseEntered( e->{
-            if(status == 0) {
-                this.getChildren().clear();
-                this.getChildren().add(cursor);
+        this.setOnMouseEntered(e -> {
+            if (status == 0) {
+                block.setOpacity(0.8); // 将透明度设置为70%来实现变暗效果
             }
         });
-        this.setOnMouseExited( e->{
-            if(status == 0){
+
+        this.setOnMouseExited(e -> {
+            if (status == 0) {
+                block.setOpacity(1.8); // 恢复透明度
+            }
+        });
+
+        // block 按下变换图像
+        this.setOnMousePressed( e->{
+            if(status == 0 && !isPress && e.getButton() == MouseButton.PRIMARY) {
                 this.getChildren().clear();
-                this.getChildren().add(block);
+                this.getChildren().add(cursor);
+                isPress = true;
+            }
+        });
+
+        // block 按下后松开变换图像
+        this.setOnMouseMoved( e->{
+            if(status == 0 && isPress){
+                this.getChildren().clear();
+                this.getChildren().add(empty);
             }
         });
     }
