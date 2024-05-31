fe51f4e9fc30d3867201ffe47995303b23c49a05
TimewornDream <2497505099@qq.com>
2024-06-01 00:44:14 +0800

    feat:完成地图初始化

--- src/GameBlock.java
+++ src/GameBlock.java
@@ -6,7 +6,9 @@ public class GameBlock extends StackPane {
     public static int edgeLength = 40;
     private int status = 0;
     private boolean isPress = false;
-    GameBlock(){
+    private int type = 0;
+
+    GameBlock() {
         super();
         this.setMaxSize(edgeLength, edgeLength);
 
@@ -60,12 +62,15 @@ public class GameBlock extends StackPane {
         this.setOnMouseExited(e -> {
             if (!isPress) {
                 this.getChildren().get(0).setOpacity(1.8);
+            } else if (status == 0) {
+                this.getChildren().clear();
+                this.getChildren().add(this.getImageWithType());
             }
         });
 
         // block 按下变换图像
-        this.setOnMousePressed( e->{
-            if(status == 0 && !isPress && e.getButton() == MouseButton.PRIMARY) {
+        this.setOnMousePressed(e -> {
+            if (status == 0 && !isPress && e.getButton() == MouseButton.PRIMARY) {
                 this.getChildren().clear();
                 this.getChildren().add(press);
                 isPress = true;
@@ -73,11 +78,38 @@ public class GameBlock extends StackPane {
         });
 
         // block 按下后松开变换图像
-        this.setOnMouseMoved( e->{
-            if(status == 0 && isPress){
+        this.setOnMouseMoved(e -> {
+            if (status == 0 && isPress) {
                 this.getChildren().clear();
-                this.getChildren().add(empty);
+                this.getChildren().add(this.getImageWithType());
             }
         });
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public ImageView getImageWithType() {
+        ImageView imageView = switch (type) {
+            case 0 -> new ImageView("./img/empty_block.png");
+            case 1 -> new ImageView("./img/block_1.png");
+            case 2 -> new ImageView("./img/block_2.png");
+            case 3 -> new ImageView("./img/block_3.png");
+            case 4 -> new ImageView("./img/block_4.png");
+            case 5 -> new ImageView("./img/block_5.png");
+            case 6 -> new ImageView("./img/block_6.png");
+            case 7 -> new ImageView("./img/block_7.png");
+            case 8 -> new ImageView("./img/block_8.png");
+            case 9 -> new ImageView("./img/block_bomb.png");
+            default -> new ImageView();
+        };
+        imageView.setFitWidth(edgeLength);
+        imageView.setFitHeight(edgeLength);
+        return imageView;
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -5,7 +5,12 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameWindow extends Stage {
+    private int numOfBomb;
     private int mapWidth;
     private int mapHeight;
     public GameWindow(String difficulty) {
@@ -28,14 +33,17 @@ public class GameWindow extends Stage {
             case "初级":
                 mapWidth = 8;
                 mapHeight = 8;
+                numOfBomb = 10;
                 break;
             case "中级":
                 mapWidth = 16;
-                mapHeight =16;
+                mapHeight = 16;
+                numOfBomb = 40;
                 break;
             case "高级":
                 mapWidth = 30;
-                mapHeight = 30;
+                mapHeight = 16;
+                numOfBomb = 99;
                 break;
         }
         int width = calculateWidth(mapWidth);
@@ -52,6 +60,7 @@ public class GameWindow extends Stage {
             }
         }
 
+        initMap(blocks);
 
         gameRoot.setAlignment(Pos.TOP_CENTER);
         gameRoot.getChildren().addAll(top, center);
@@ -67,4 +76,43 @@ public class GameWindow extends Stage {
     private static int calculateHeight (int height) {
         return 40*height+160;
     }
+    public void initMap(GameBlock[][] blocks){
+        Random random = new Random();
+        List<Integer> bombIndexes = new ArrayList<>();
+
+        // 随机生成地雷位置
+        while (bombIndexes.size() < numOfBomb) {
+            int index = random.nextInt(mapWidth * mapHeight);
+            if (!bombIndexes.contains(index)) {
+                bombIndexes.add(index);
+            }
+        }
+
+        // 将地雷位置设置为地雷
+        for (int index : bombIndexes) {
+            int x = index % mapWidth;
+            int y = index / mapWidth;
+            blocks[x][y].setType(9); // 9 表示地雷
+        }
+
+        // 设置周围数字
+        for (int i = 0; i < mapWidth; i++) {
+            for (int j = 0; j < mapHeight; j++) {
+                if (blocks[i][j].getType() != 9) { // 不是地雷的方块
+                    int count = 0;
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            int newX = i + dx;
+                            int newY = j + dy;
+                            if (newX >= 0 && newX < mapWidth && newY >= 0 && newY < mapHeight && blocks[newX][newY].getType() == 9) {
+                                count++;
+                            }
+                        }
+                    }
+                    blocks[i][j].setType(count); // 设置周围地雷数量
+                }
+            }
+        }
+    }
+
 }
