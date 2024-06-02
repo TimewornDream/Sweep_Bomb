08ab7c0988ac1d68c93a36bfb48801069115cd06
TimewornDream <2497505099@qq.com>
2024-06-02 21:09:44 +0800

    feat:完成按钮按下时图片转换

--- src/FireflyButton.java
+++ src/FireflyButton.java
@@ -4,13 +4,47 @@ public class FireflyButton extends Button {
     private int status = 0;
     FireflyButton(){
         super();
-        this.setMinWidth(70);
-        this.setMaxHeight(70);
+        this.setMinWidth(74);
+        this.setMaxHeight(74);
+        this.setInitStyle();
+        this.setOnMousePressed(e->{
+            this.setClickedStyle();
+        });
+        this.setOnMouseReleased(e->{
+            this.setInitStyle();
+        });
+    }
+    private void setInitStyle(){
         this.setStyle(
-                "-fx-background-image: url(./img/firefly.png);" +
+                getStyleLineWithType(0) +
                         "-fx-background-size: cover;" +
-                        "-fx-border-radius: 4;" +
-                        "-fx-border-color: #8ca386"
+                        "-fx-border-color: #8ca386;" +
+                        "-fx-border-width: 2"
         );
+    }
+    private void setClickedStyle(){
+        this.setStyle(
+                getStyleLineWithType(1) +
+                        "-fx-background-size: cover;" +
+                        "-fx-border-color: #8ca386;" +
+                        "-fx-border-width: 2"
+        );
+    }
+
+    // type 为0表示未按下，为1表示按下
+    private String getStyleLineWithType(int x){
+        String end;
+        if (x == 0) {
+            end = ".png);";
+        } else {
+            end = "_onClick.png);";
+        }
+        return switch (status) {
+            case 0 -> "-fx-background-image: url(./img/firefly" + end;
+            case 1 -> "-fx-background-image: url(./img/firefly_press" + end;
+            case 2 -> "-fx-background-image: url(./img/firefly_win" + end;
+            case 3 -> "-fx-background-image: url(./img/firefly_lose" + end;
+            default -> "err";
+        };
     }
 }
